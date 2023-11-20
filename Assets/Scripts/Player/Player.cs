32f81739f508d488c0c0f430d735594636f3e3a7
ATHENALATOP\athen <athenaabcxyz@gmail.com>
2023-11-20 16:22:38 +0700

    Fixing project

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+#region REQUIRE COMPONENTS
+[RequireComponent(typeof(Health))]
+[RequireComponent(typeof(PlayerControl))]
+[RequireComponent(typeof(MovementByVelocityEvent))]
+[RequireComponent(typeof(MovementByVelocity))]
+[RequireComponent(typeof(MovementToPositionEvent))]
+[RequireComponent(typeof(MovementToPosition))]
+[RequireComponent(typeof(IdleEvent))]
+[RequireComponent(typeof(Idle))]
+[RequireComponent(typeof(AimWeaponEvent))]
+[RequireComponent(typeof(AimWeapon))]
+[RequireComponent(typeof(FireWeaponEvent))]
+[RequireComponent(typeof(FireWeapon))]
+[RequireComponent(typeof(SetActiveWeaponEvent))]
+[RequireComponent(typeof(ActiveWeapon))]
+[RequireComponent(typeof(WeaponFiredEvent))]
+[RequireComponent(typeof(ReloadWeaponEvent))]
+[RequireComponent(typeof(ReloadWeapon))]
+[RequireComponent(typeof(WeaponReloadedEvent))]
+[RequireComponent(typeof(AnimatePlayer))]
+[RequireComponent(typeof(SortingGroup))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+[DisallowMultipleComponent]
+#endregion REQUIRE COMPONENTS
+
+
+
+public class Player : MonoBehaviour
+{
+    [HideInInspector] public PlayerDetailsSO playerDetails;
+    [HideInInspector] public Health health;
+    [HideInInspector] public MovementByVelocityEvent movementByVelocityEvent;
+    [HideInInspector] public MovementToPositionEvent movementToPositionEvent;
+    [HideInInspector] public IdleEvent idleEvent;
+    [HideInInspector] public AimWeaponEvent aimWeaponEvent;
+    [HideInInspector] public FireWeaponEvent fireWeaponEvent;
+    [HideInInspector] public SetActiveWeaponEvent setActiveWeaponEvent;
+    [HideInInspector] public ActiveWeapon activeWeapon;
+    [HideInInspector] public WeaponFiredEvent weaponFiredEvent;
+    [HideInInspector] public ReloadWeaponEvent reloadWeaponEvent;
+    [HideInInspector] public WeaponReloadedEvent weaponReloadedEvent;
+    [HideInInspector] public SpriteRenderer spriteRenderer;
+    [HideInInspector] public Animator animator;
+
+    public List<Weapon> weaponList = new List<Weapon>();
+
+    private void Awake()
+    {
+        // Load components
+        health = GetComponent<Health>();
+        movementByVelocityEvent = GetComponent<MovementByVelocityEvent>();
+        movementToPositionEvent = GetComponent<MovementToPositionEvent>();
+        idleEvent = GetComponent<IdleEvent>();
+        aimWeaponEvent = GetComponent<AimWeaponEvent>();
+        fireWeaponEvent = GetComponent<FireWeaponEvent>();
+        setActiveWeaponEvent = GetComponent<SetActiveWeaponEvent>();
+        activeWeapon = GetComponent<ActiveWeapon>();
+        weaponFiredEvent = GetComponent<WeaponFiredEvent>();
+        reloadWeaponEvent = GetComponent<ReloadWeaponEvent>();
+        weaponReloadedEvent = GetComponent<WeaponReloadedEvent>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    /// <summary>
+    /// Initialize the player
+    /// </summary>
+    public void Initialize(PlayerDetailsSO playerDetails)
+    {
+        this.playerDetails = playerDetails;
+
+        //Create player starting weapons
+        CreatePlayerStartingWeapons();
+
+
+        // Set player starting health
+        SetPlayerHealth();
+    }
+
+    /// <summary>
+    /// Set the player starting weapon
+    /// </summary>
+    private void CreatePlayerStartingWeapons()
+    {
+        // Clear list
+        weaponList.Clear();
+
+        // Populate weapon list from starting weapons
+        foreach (WeaponDetailsSO weaponDetails in playerDetails.startingWeaponList)
+        {
+            // Add weapon to player
+            AddWeaponToPlayer(weaponDetails);
+        }
+    }
+
+    /// <summary>
+    /// Set player health from playerDetails SO
+    /// </summary>
+    private void SetPlayerHealth()
+    {
+        health.SetStartingHealth(playerDetails.playerHealthAmount);
+    }
+
+    /// <summary>
+    /// Add a weapon to the player weapon dictionary
+    /// </summary>
+    public Weapon AddWeaponToPlayer(WeaponDetailsSO weaponDetails)
+    {
+        Weapon weapon = new Weapon() { weaponDetails = weaponDetails, weaponReloadTimer = 0f, weaponClipRemainingAmmo = weaponDetails.weaponClipAmmoCapacity, weaponRemainingAmmo = weaponDetails.weaponAmmoCapacity, isWeaponReloading = false };
+
+        // Add the weapon to the list
+        weaponList.Add(weapon);
+
+        // Set weapon position in list
+        weapon.weaponListPosition = weaponList.Count;
+
+        // Set the added weapon as active
+        setActiveWeaponEvent.CallSetActiveWeaponEvent(weapon);
+
+        return weapon;
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# 2DDungeonRoguelike
+This is the project for learning Unity Engine at beginner-intermediate levels.
+
+*Update 22-10-2023:
+- Setup editor tool for creating dungeon graph
+- Setup various room and dungeon templates
+- Setup main game scene, cinemachine
+- Complete random dungeon generator and set up GameManager
+- Setup Player class (ongoing)
+
+*Update 09-11-2023:
+- Add project document
+- Add player prefabs
+- Configure Animation
+
+*Update 10-11-2023:
+- Config player control and instanciation
+- Config cinemachine to target both player and mouse cursor (aim direction)
+
+*Update 11-11-2023:
+- Add door interactive function:
+  + Appear when player get close
+  + Open when player walk pass
+  + Close when player enter a room with enemies
+  + Re-open after player clear the room
+  + Fade out visual behind the door when closed
+  + Appear the visual behind the door when open
+
+*Update 1-11-2023:
+- Implement object pooling
+
+*Update 20-11-2023
+- Configure weapon and ammo system
+- Configure weapons and ammo types variant
+
